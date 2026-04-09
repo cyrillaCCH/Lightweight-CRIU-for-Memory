344876a901aebecb5d6b420ff90fce94291b788a
cyrillaCCH <cch92423@gmail.com>
2026-04-09 18:01:51 +0800

    skip collect_children in mem_dump (some Android may not enable CONFIG_PROC_CHILDREN)

--- criu/seize.c
+++ criu/seize.c
@@ -971,9 +971,11 @@ static int collect_task(struct pstree_item *item)
 		goto err_close;
 
 	/* Depth-first search (DFS) is used for traversing a process tree. */
-	ret = collect_loop(item, collect_children);
-	if (ret < 0)
-		goto err_close;
+	if (opts.mode != CR_MEM_DUMP) {
+		ret = collect_loop(item, collect_children);
+		if (ret < 0)
+			goto err_close;
+	}
 
 	if ((item->pid->state == TASK_DEAD) && !list_empty(&item->children)) {
 		pr_err("Zombie with children?! O_o Run, run, run!\n");
